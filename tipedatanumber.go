26bd7f3f7deb6126632197a39c524f95f8c4f3ec
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2021-12-19 09:08:13 +0700

    belajar golang basic

---
+++ tipedatanumber.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("tigapuluh = ", 30)
+	fmt.Println("Satukomlima = ", 1.5)
+}
---
+++ tipedatastring.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+	// hitung string
+	fmt.Println(len("hello"))
+	// ambil string
+	fmt.Println("hello"[2])
+
+}
