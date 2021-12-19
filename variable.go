eb67d885f721b5f71bbd5458dc224b82932bdf51
afdhalluthfi09 <afdhal.luthfi@students.amikom.ac.id>
2021-12-19 09:16:20 +0700

    baru

--- variable.go
+++ variable.go
@@ -5,7 +5,19 @@ import "fmt"
 func main() {
 	var name string
 
+	// jika banyak variable
+	var (
+		firstname string
+		lastname  string
+		age       int
+		address   string
+	)
+	firstname = "Afdhal"
+	lastname = "Luthfi"
+	age = 28
+	address = "Bantul"
 	name = "Afdhal Luthfi"
 
 	fmt.Println(name)
+	fmt.Println(name, firstname, lastname, age, address)
 }
